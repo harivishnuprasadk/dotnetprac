054388a2ebc7aad08a5e1ac047904332ad725664
hvp <harivishnuprasadk@gmail.com>
2023-01-23 23:45:15 +0530

    Updated Crud app

---
+++ CrudDemo/CrudDemo/Models/IssuesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace CrudDemo.Models
+{
+    public class IssuesDTO
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public string IssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+}
+
